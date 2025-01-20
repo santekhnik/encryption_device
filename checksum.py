339a111a5d9ebc9c8bc271edeee46c0f5f26d000
danylo <danylohrabar@gmail.com>
2025-01-20 19:08:34 +0200

    fixed

--- checksum.py
+++ checksum.py
@@ -1,13 +1,18 @@
-def calculate_bcc(data: bytes) -> int:
-    checksum = 0
+def calculate_bcc(data):
+    """
+    Обчислює BCC (Block Check Character) для заданого блоку даних.
+
+    :param data: Масив байтів (наприклад, b'123' або [0x31, 0x32, 0x33])
+    :return: Однобайтове значення BCC
+    """
+    bcc = 0x00  # Початкове значення CRC
     for byte in data:
-        checksum = (checksum + byte) % 256
-    return checksum
+        bcc ^= byte  # Виконуємо XOR з кожним байтом
+    return bcc
 
-if __name__ == '__main__':
-    test_data = b'\x12\x34\x56\x78'
-    checksum = calculate_bcc(test_data)
-    print(f'Data: {test_data}')
-    print(f'BCC Checksum: 0x{checksum:02X}')
 
-    # Додати коментарі
+test_data = b'\x12\x34\x56\x78'
+checksum = calculate_bcc(test_data)
+print(f'Data: {test_data}')
+print(f'BCC Checksum: 0x{checksum:02X}')
+
