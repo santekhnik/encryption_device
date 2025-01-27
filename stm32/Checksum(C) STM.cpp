17f3f8ce3a91b16e99e8c27019808826010b4d90
Scar3cr0www <vazquezchannel219@gmail.com>
2025-01-27 21:07:02 +0200

    Add files via upload

---
+++ stm32/Checksum(C) STM.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+unsigned char calculate_bcc(const unsigned char* data, size_t length) {
+    /*
+     * Обчислює BCC (Block Check Character) для заданого блоку даних.
+     *
+     * data: Масив байтів
+     * length: Довжина масиву даних
+     * return: Однобайтове значення BCC
+     */
+    unsigned char bcc = 0x00; // Початкове значення BCC
+    for (size_t i = 0; i < length; i++) {
+        bcc ^= data[i]; // Виконуємо XOR з кожним байтом
+    }
+    return bcc;
+}
+
+int main() {
+    unsigned char test_data[] = { 0x12, 0x34, 0x56, 0x78 };
+    size_t length = sizeof(test_data) / sizeof(test_data[0]);
+
+    unsigned char checksum = calculate_bcc(test_data, length);
+
+    printf("Data: ");
+    for (size_t i = 0; i < length; i++) {
+        printf("0x%02X ", test_data[i]);
+    }
+    printf("\nBCC Checksum: 0x%02X\n", checksum);
+
+    return 0;
+}
